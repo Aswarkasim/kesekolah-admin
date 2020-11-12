27b6a1107507dd61d37940e835ae6b2129e460f2
aswarkasim <aswarkasim@gmail.com>
2020-11-13 06:01:06 +0700

    is_boolean

--- application/modules/api/controllers/Anak.php
+++ application/modules/api/controllers/Anak.php
@@ -28,4 +28,54 @@ class Anak extends REST_Controller
     }
     $this->response($child, REST_Controller::HTTP_OK);
   }
+
+  function index_post()
+  {
+
+    $this->load->helper('string');
+
+    $data = [
+      'id_anak'       => random_string(),
+      'nama_anak'     => $this->post('nama_anak'),
+      'umur'          => $this->post('umur'),
+      'id_user'       => $this->post('id_user'),
+      'id_sekolah'    => $this->post('id_sekolah')
+    ];
+    $insert = $this->db->insert('tbl_anak', $data);
+    if ($insert) {
+      $this->response(['error' => 'false', 'data' => $data], REST_Controller::HTTP_OK);
+    } else {
+      $this->response(['error' => 'true'], 502);
+    }
+  }
+
+  function is_jemput_put()
+  {
+    $id_anak = $this->put('id_anak');
+    $data = [
+      'is_jemput' => $this->put('is_jemput')
+    ];
+    $this->db->where('id_anak', $id_anak);
+    $update = $this->db->update('tbl_anak', $data);
+    if ($update) {
+      $this->response(['error' => 'false', 'data' => $data], 200);
+    } else {
+      $this->response(['error' => 'true'], 502);
+    }
+  }
+
+  function is_ready_put()
+  {
+    $id_anak = $this->put('id_anak');
+    $data = [
+      'is_ready' => $this->put('is_ready')
+    ];
+    $this->db->where('id_anak', $id_anak);
+    $update = $this->db->update('tbl_anak', $data);
+    if ($update) {
+      $this->response(['error' => 'false', 'data' => $data], 200);
+    } else {
+      $this->response(['error' => 'true'], 502);
+    }
+  }
 }
