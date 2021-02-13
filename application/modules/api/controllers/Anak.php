151589eaa14d7b75fff1141c200f7cc0de055796
aswarkasim <aswarkasim@gmail.com>
2021-02-13 14:14:15 +0700

    put

--- application/modules/api/controllers/Anak.php
+++ application/modules/api/controllers/Anak.php
@@ -104,6 +104,7 @@ class Anak extends REST_Controller
   {
     $id_anak = $this->put('id_anak');
     $data = [
+      'id_anak'   => $id_anak,
       'is_ready' => $this->put('is_ready')
     ];
     $this->db->where('id_anak', $id_anak);
@@ -122,27 +123,26 @@ class Anak extends REST_Controller
   }
 
 
-  function is_ready_post()
-  {
-    $id_anak = $this->post('id_anak');
-    $data = [
-      'id_anak'   => $id_anak,
-      'is_ready' => $this->post('is_ready')
-    ];
-    $this->db->where('id_anak', $id_anak);
-    $update = $this->db->update('tbl_anak', $data);
-    if ($update) {
-      $this->response([
-        'error' => false,
-        'message' => 'Data siap anak berhasil diubah',
-      ], 200);
-    } else {
-      $this->response([
-        'error' => true,
-        'message' => 'Data siap anak gagal diubah',
-      ], 502);
-    }
-  }
+  // function is_ready_post()
+  // {
+  //   $id_anak = $this->post('id_anak');
+  //   $data = [
+  //     'is_ready' => $this->post('is_ready')
+  //   ];
+  //   $this->db->where('id_anak', $id_anak);
+  //   $update = $this->db->update('tbl_anak', $data);
+  //   if ($update) {
+  //     $this->response([
+  //       'error' => false,
+  //       'message' => 'Data siap anak berhasil diubah',
+  //     ], 200);
+  //   } else {
+  //     $this->response([
+  //       'error' => true,
+  //       'message' => 'Data siap anak gagal diubah',
+  //     ], 502);
+  //   }
+  // }
 
   function is_hadir_put()
   {
